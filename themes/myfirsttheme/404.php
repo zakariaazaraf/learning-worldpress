cf325a6de9ec06210cc021edd977c0587de4b208
zakaria <zakariaazaraf@gmail.com>
2021-05-23 21:22:06 +0100

    add the 404 Page

---
+++ themes/myfirsttheme/404.php
@@ -0,0 +1,9 @@
+<?php get_header(  ); ?>
+<div class="container">
+    <h1 class='text-center font-weight-bold display-2 text-capitalize my-5'>404 Page not found</h1>
+    <img src="<?php echo get_template_directory_uri(  ); ?>/assets/images/404.jpg" alt="page not found">
+    <div class="page-not-found mx-md-5 display-4 my-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit mollitia, veniam facilis suscipit doloremque labore maiores eius culpa assumenda quae.
+    </div>
+</div>
+<?php get_footer(); ?>
